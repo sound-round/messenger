077293fdfbbf5d9e5d34b5d19ee784c502a95ef5
denis <yudaev1@gmail.com>
2021-05-28 13:48:08 +0300

    Rewrite responses in classes in server.py

--- messenger/server/server.py
+++ messenger/server/server.py
@@ -6,13 +6,18 @@
 # 3) /sendMessage  - send message to some user - params: auth_token, to_user_id, message: String
 # 4) /readMessages - receive messages since date (polling), should be called each 1sec
 #                  - params: auth_token, since_date
-#                  - result: list of Message(message: String, date: Date, from_user_id: Int)
+#                  - result: messages:[Message(message: String, date: Date, from_user_id: Int)], current_time:Date
+#                  - additionally updates last_active value for user
 # 5) /getUser - get info about user - params: auth_token, user_id
 #             - result: login: String, last_active: Date, avatar_url: String
 # 6) /getUserByLogin - get info about user - params: auth_token, login,
 #             - result: login: String, last_active: Date, avatar_url: String, user_id: String
 # 7) /setAvatar - set avatar - params: auth_token, avatar_url
+#
+# Data types:
+# Date - unix time in milliseconds integer
 
+import datetime
 import socketserver
 from http.server import BaseHTTPRequestHandler
 import json
@@ -21,10 +26,16 @@ import validators
 
 # registered_users = []  # TODO store users as User objects with login, password, user_id
 # auth_token_store = []        # TODO store user's auth_token_store as objects with user_id, auth_token_store {user_id = 10, auth_token_store="fhhhdas123"}
+messages = [] # TODO replace with some fancy store\manager class
+
 
 registered_users = RegisteredUsersManager()
 auth_token_store = AuthTokenManager()
 
+#TODO replace self.wfile.write with self.writeResponse(response)
+
+def currentTime():
+    return int(datetime.datetime.utcnow().timestamp() * 1000)
 
 # TODO extract to separate file
 class Response(object):
@@ -46,7 +57,32 @@ class LoginResponse(Response):
         self.auth_token = auth_token
 
 
+class GetUser(Response):
+
+    def __init__(self, user_login, avatar, last_active):
+        super().__init__("ok")
+        self.user_login = user_login
+        self.avatar = avatar
+        self.last_active = last_active
+
+
+class ReadMessagesResponse(Response):
+
+    def __init__(self, messages, current_date):
+        super().__init__("ok")
+        self.messages = messages
+        self.current_date = current_date
+
+
+
 class ServerHandler(BaseHTTPRequestHandler):
+
+    def get_post_body(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        content_len = int(self.headers.get('Content-Length'))
+        return (self.rfile.read(content_len)).decode()
 
     def writeResponse(self, response):
         self.wfile.write(str.encode(toJSON(response)))
@@ -66,11 +102,7 @@ class ServerHandler(BaseHTTPRequestHandler):
             self.handle_set_avatar()
 
     def handle_register(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        content_len = int(self.headers.get('Content-Length'))
-        post_body = (self.rfile.read(content_len)).decode()
+        post_body = self.get_post_body()
         login = json.loads(post_body).get('login')
         password = json.loads(post_body).get('password')
         print(post_body)
@@ -97,28 +129,24 @@ class ServerHandler(BaseHTTPRequestHandler):
         registered_users.add_user(user)
         registered_users.show_users()
 
-        self.wfile.write(str.encode("{\"result\" : \"user_registered\"}"))
+        self.writeResponse(Response(("user_registered")))
 
     def handle_login(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        content_len = int(self.headers.get('Content-Length'))
-        post_body = (self.rfile.read(content_len)).decode()
+        post_body = self.get_post_body()
         login = json.loads(post_body)['login']
         password = json.loads(post_body)['password']
         # TODO check password
         # TODO handle other errors: missing password, missing login, etc...
         if len(login) == 0:
-            self.wfile.write(str.encode("{\"result\" : \"login_is_missing\"}"))
+            self.writeResponse(Response("login_is_missing"))
             return
         if len(password) == 0:
-            self.wfile.write(str.encode("{\"result\" : \"password_is_missing\"}"))
+            self.writeResponse(Response("password_is_missing"))
             return
         if registered_users.is_login_in_store(login):
             user = registered_users.get_user_by_login(login)
             if not user.check_password(password):
-                self.wfile.write(str.encode("{\"result\" : \"wrong_password\"}"))
+                self.writeResponse(Response("wrong_password"))
                 return
 
             auth_token = generate_auth_token()
@@ -127,97 +155,83 @@ class ServerHandler(BaseHTTPRequestHandler):
             auth_token_store.show_user_tokens()
             return
 
-        self.wfile.write(str.encode("{\"result\" : \"unknown_login\"}"))
+        self.writeResponse(Response("unknown_login"))
 
     def handle_send_message(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        content_len = int(self.headers.get('Content-Length'))
-        post_body = (self.rfile.read(content_len)).decode()
+        post_body = self.get_post_body()
         auth_token = json.loads(post_body)['auth_token']
         to_user_id = json.loads(post_body)['to_user_id']
         message = json.loads(post_body)['message']
-        if not auth_token_store.is_token_in_store(auth_token):
-            self.wfile.write(str.encode(
-                "{\"result\" : \"unknown_auth_token\"}"
-            ))
+        user = auth_token_store.get_user_by_auth_token(auth_token)
+        if user is None:
+            self.writeResponse(Response("unknown_auth_token"))
             return
+
         if not registered_users.is_id_in_store(to_user_id):
-            self.wfile.write(str.encode(
-                "{\"result\" : \"user_is_missing\"}"
-            ))
+            self.writeResponse(Response("user_is_missing"))
             return
         if len(message) == 0:
-            self.wfile.write(str.encode("{\"result\" : \"message_is_missing\"}"))
+            self.writeResponse(Response("message_is_missing"))
             return
 
-        self.wfile.write(str.encode("{\"result\" : \"message_has_delivered\"}"))
+        sself.writeResponse(Response("message_has_delivered"))
 
     def handle_read_message(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        content_len = int(self.headers.get('Content-Length'))
-        post_body = (self.rfile.read(content_len)).decode()
-        since_date = json.loads(post_body)['auth_token']
-        message = json.loads(post_body)['message']
+        post_body = self.get_post_body()
+        auth_token = json.loads(post_body)['auth_token']
+        since_date = json.loads(post_body)['since_date']
 
-        self.wfile.write(str.encode("{\"result\" : \"not implemented\"}"))
+        user = auth_token_store.get_user_by_auth_token(auth_token) # переделать
+        ct = currentTime()
+
+        if user is not None:
+            user.last_active = ct
+
+        messagesToRead = []
+        for m in messages:
+            if m.to_user_id == user.user_id and m.date >= since_date:
+                messagesToRead.append(m)
+
+        self.writeResponse(ReadMessagesResponse(messagesToRead, ct))
 
     # setAvatar - set avatar - params: auth_token, avatar_url
     def handle_set_avatar(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        content_len = int(self.headers.get('Content-Length'))
-        post_body = (self.rfile.read(content_len)).decode()
+        post_body = self.get_post_body()
         auth_token = json.loads(post_body)['auth_token']
         avatar_url = json.loads(post_body)['avatar_url']
         if not auth_token_store.is_token_in_store(auth_token):
-            self.wfile.write(str.encode(
-                "{\"result\" : \"unknown_auth_token\"}"
-            ))
+            self.writeResponse(Response("unknown_auth_token"))
             return
         if len(avatar_url) == 0:
-            self.wfile.write(str.encode("{\"result\" : \"url_is_missing\"}"))
+            self.writeResponse(Response("url_is_missing"))
             return
         if not validators.url(avatar_url):
-            self.wfile.write(str.encode("{\"result\" : \"url_is_not_valid\"}"))
+            self.writeResponse(Response("url_is_not_valid"))
             return
         # http request url
         user_id = auth_token_store.get_user_by_auth_token(auth_token).get_id()
         user = registered_users.get_user_by_id(user_id)
         user.set_avatar(avatar_url)
-        self.wfile.write(str.encode("{\"result\" : \"avatar_has_been_set\"}"))
+        self.writeResponse(Response("avatar_has_been_set"))
 
     # 5) /getUser - get info about user - params: auth_token, user_id
     #             - result: login: String, last_active: Date, avatar_url: String
     def handle_get_user(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        content_len = int(self.headers.get('Content-Length'))
-        post_body = (self.rfile.read(content_len)).decode()
+        post_body = self.get_post_body()
         auth_token = json.loads(post_body)['auth_token']
         user_id_to_get = json.loads(post_body)['user_id_to_get']
         if not auth_token_store.is_token_in_store(auth_token):
-            self.wfile.write(str.encode(
-                "{\"result\" : \"unknown_auth_token\"}"
-            ))
+            self.writeResponse(Response("unknown_auth_token"))
             return
         if not registered_users.is_id_in_store(user_id_to_get):
-            self.wfile.write(str.encode(
-                "{\"result\" : \"user_is_missing\"}"
-            ))
+            self.writeResponse(Response("user_is_missing"))
             return
         user = registered_users.get_user_by_id(user_id_to_get)
         last_active = None  # How should it be implemented?
 
-        self.wfile.write(str.encode(
-            "\"result\" :\n \"Login: {}\nLast active: {}\nAvatar url: {}\"".format(
-                user.get_login(), last_active, user.get_avatar(),
-            )
+# TODO rewrite response under in class
+        self.writeResponse(GetUser(
+            user.get_login(), user.get_avatar(), last_active,
         ))
 
 
