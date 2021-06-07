971e6fdb6a5992d9f7658479289e1d669f3d5e35
denis <yudaev1@gmail.com>
2021-06-07 16:59:45 +0300

    Refactor \client

---
+++ messenger/client/network.py
@@ -0,0 +1,43 @@
+
+import requests
+import json
+
+
+
+
+# типы клиента
+# 1) Command line client -
+# 2) Desktop ui client - tkinter,
+# 3) web client - some python web libraries?
+
+
+import tkinter as tk
+from tkinter import LEFT, RIGHT, BOTTOM, StringVar
+from tkinter import messagebox as mb
+
+def do_actual_register(login, password):
+    request_output = '\n'.join([
+        "do actual register network request...",
+        f'login: {login}',
+        f'password: {password}',
+    ])
+    print(request_output)
+    request_json = json.dumps({'login': login, 'password': password})
+    response = requests.post("http://localhost:8080/register", data=request_json)
+    response_text = response.text  # How does it work?
+    print(response_text)
+    return response_text
+
+
+def do_actual_login(login, password):
+    request_output = '\n'.join([
+        "do actual login network request...",
+        f'login: {login}',
+        f'password: {password}',
+    ])
+    print(request_output)
+    request_json = json.dumps({'login': login, 'password': password})
+    response = requests.post("http://localhost:8080/login", data=request_json)
+    response_text = response.text
+    print(response_text)
+    return response_text
--- messenger/client/test_client.py
+++ messenger/client/test_client.py
@@ -1,6 +1,6 @@
-import urllib.request
 import requests
 import json
+from messenger.client import network
 
 ##contents = urllib.request.urlopen("http://localhost:8080/register").read()
 
@@ -34,23 +34,22 @@ def get_quit_button():
 
 root = tk.Tk()
 root.title("messenger")
-root.geometry('300x130')
+root.geometry('300x150')
 
 
-menu = tk.Menu(root)
-root.config(menu=menu)
-filemenu = tk.Menu(menu)
-menu.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="Quit", command=callback)
+def add_menu():
+    menu = tk.Menu(root)
+    root.config(menu=menu)
+    filemenu = tk.Menu(menu)
+    menu.add_cascade(label="File", menu=filemenu)
+    filemenu.add_command(label="Quit", command=callback)
 
-#frame = tk.Frame(root)
-#frame.pack()
+add_menu()
 
 #TODO refactor, separate UI and Logic and network to multiple files
 #TODO advanced - investigate slow response times (measure and log some basic times)
 
 def display_register_ui():
-    #print("Do register, todo show login and password")
     remove_all(root)
     login_label = tk.Label(root, text="Login")
     login_entry = tk.Entry(root)
@@ -58,26 +57,16 @@ def display_register_ui():
     password_entry = tk.Entry(root)
     result_text = StringVar()
     result_label = tk.Label(root, textvariable=result_text)
-
-    def doActualRegister():
+    add_menu()
+    def register():
         login = login_entry.get()
         password = password_entry.get()
-        request_output = '\n'.join([
-            "do actual register network request...",
-            f'login: {login}',
-            f'password: {password}',
-        ])
-        print(request_output)
-        request_json = json.dumps({'login': login, 'password': password})
-        response = requests.post("http://localhost:8080/register", data=request_json)
-        response_text = response.text  # How does it work?
-        print(response_text)
-
+        response_text = network.do_actual_register(login, password)
         result_text.set(''.join(["result= ", json.loads(response_text)['result']]))
 
     register_button = tk.Button(root,
                                text="do Register",
-                               command=doActualRegister)
+                               command=register)
     login_label.grid(row=0,)
     login_entry.grid(row=0, column=1)
     password_label.grid(row=1)
@@ -87,9 +76,6 @@ def display_register_ui():
     get_quit_button().grid(row=3)
 
 
-#def login():
- #   print("Do login, todo show login and password")
- #   removeAll(root)
     #TODO implement login button-logic similar to register button
 
 
@@ -101,26 +87,16 @@ def display_login_ui():
     password_entry = tk.Entry(root)
     result_text = StringVar()
     result_label = tk.Label(root, textvariable=result_text)
-
-    def do_actual_login():
+    add_menu()
+    def login():
         login = login_entry.get()
         password = password_entry.get()
-        request_output = '\n'.join([
-            "do actual login network request...",
-            f'login: {login}',
-            f'password: {password}',
-        ])
-        print(request_output)
-        request_json = json.dumps({'login': login, 'password': password})
-        response = requests.post("http://localhost:8080/login", data=request_json)
-        response_text = response.text
-        print(response_text)
-
+        response_text = network.do_actual_login(login, password)
         result_text.set(''.join(["result= ", json.loads(response_text)['result']]))
 
     login_button = tk.Button(root,
                                text="do Login",
-                               command=do_actual_login)
+                               command=login)
     login_label.grid(row=0,)
     login_entry.grid(row=0, column=1)
     password_label.grid(row=1)
@@ -128,8 +104,6 @@ def display_login_ui():
     login_button.grid(row=2)
     result_label.grid(row=2, column=1)
     get_quit_button().grid(row=3)
-
-
 
 
 login_button = tk.Button(root,
@@ -144,6 +118,5 @@ register_button = tk.Button(root,
 login_button.grid(row=0, padx=4)
 register_button.grid(row=0, column=1, pady=10)
 get_quit_button().grid(row=1)
-
 
 root.mainloop()
