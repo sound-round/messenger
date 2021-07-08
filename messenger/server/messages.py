9a12c2c2b49abfa4008d6bf7938a29ae0fe64ec0
denis <yudaev1@gmail.com>
2021-07-08 10:05:33 +0300

    Update messages.py

--- messenger/server/messages.py
+++ messenger/server/messages.py
@@ -1,9 +1,13 @@
-#TODO move to server folder
-
-
 class Message:
     def __init__(self, from_user_id, to_user_id, message, date):
         self.from_user_id = from_user_id
         self.to_user_id = to_user_id
         self.message = message
         self.date = date
+
+        #self.__dict__ = {
+        #        'from_user_id': self.from_user_id,
+        #        'to_user_id': self.to_user_id,
+        #        'message': self.message,
+        #        'date': self.date,
+        #    }
