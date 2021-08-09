81dcb2f96d157fa0ce579f4e91f4c887a2461ba1
denis <yudaev1@gmail.com>
2021-08-09 16:54:33 +0300

    Update sql.py

--- messenger/client/sql.py
+++ messenger/client/sql.py
@@ -1,10 +1,16 @@
 import sqlite3
 import psycopg2
+import datetime
 
 
 # def create_connection():
 #     connection = sqlite3.connect('messenger.db')
 #     return
+
+
+def current_time():
+    return datetime.datetime.utcnow().timestamp() * 1000
+
 
 def create_connection():
    # dbName = "clientDb_" + user_id
@@ -116,5 +122,66 @@ def add_message(
                     VALUES (%s, %s);
                     ''',
                     (from_user_id, new_message_id)
+                )
+            connection.commit()
+
+def send_message(
+        from_user_id,
+        to_user_id,
+        message,
+
+):
+    with create_connection() as connection:
+        with connection.cursor() as cursor:
+            date = current_time()
+            cursor.execute(
+                '''
+                INSERT INTO messages (from_user_id, to_user_id, message, date)
+                VALUES (%s, %s, %s, %s);
+                ''',
+                (
+                    from_user_id,
+                    to_user_id,
+                    message,
+                    date,
+                )
+            )
+            connection.commit()
+
+            cursor.execute(
+                '''
+                SELECT id FROM messages
+                WHERE from_user_id = %s
+                AND date = %s;
+                ''',
+                (from_user_id, date)
+            )
+
+            new_message_id = cursor.fetchone()[0]
+
+            cursor.execute(
+                '''
+                SELECT * FROM chats
+                WHERE with_user_id = %s;
+                ''',
+                (to_user_id,)
+            )
+            chat = cursor.fetchone()
+            if chat:
+                cursor.execute(
+                    '''
+                    UPDATE chats
+                    SET last_message_id = %s
+                    WHERE with_user_id = %s;
+                    ''',
+                    (new_message_id, to_user_id)
+                )
+            else:
+                cursor.execute(
+                    '''
+                    INSERT INTO chats
+                    VALUES (with_user_id, last_message_id);
+                    ''',
+                    (to_user_id, new_message_id)
                 )
             connection.commit()
